2ffc091895f7ceb2c2697edf4350e0d522ec1166
tien.doan <tien.doan@impl.vn>
2021-02-19 14:48:50 +0700

    Add open link with chrome custom tabs inside app function

---
+++ app/src/main/java/com/kira/android_base/base/supports/utils/WebUtils.kt
@@ -0,0 +1,14 @@
+package com.kira.android_base.base.supports
+
+import android.content.Context
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.net.toUri
+
+private const val CUSTOM_TAB_PACKAGE_NAME = "com.android.chrome"
+
+fun openLinkByChromeCustomTabs(context: Context, url: String) {
+    CustomTabsIntent.Builder().build().apply {
+        intent.setPackage(CUSTOM_TAB_PACKAGE_NAME)
+        launchUrl(context, url.toUri())
+    }
+}
