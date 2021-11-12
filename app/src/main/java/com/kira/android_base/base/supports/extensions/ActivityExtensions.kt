7a7b2b46ad30514e49ddafc6bddbadbe0e449f3d
tiendvyopaz <tiendv@yopaz.vn>
2021-11-12 10:12:36 +0700

    Remove RxJava, add Coroutines, refactor project

--- app/src/main/java/com/kira/android_base/base/supports/extensions/ActivityExtensions.kt
+++ app/src/main/java/com/kira/android_base/base/supports/extensions/ActivityExtensions.kt
@@ -9,7 +9,10 @@ import android.view.View
 import android.view.WindowInsetsController
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.fragment.app.FragmentManager
 
 /**
@@ -56,6 +59,49 @@ fun AppCompatActivity.showStatusBar(root: View) {
     } else {
         window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
+}
+
+fun AppCompatActivity.makeStatusBarTransparent(
+    isLightStatusBar: Boolean = true,
+    statusBarColor: Int = Color.TRANSPARENT
+) {
+    window?.apply {
+        addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        decorView.systemUiVisibility =
+            if (isLightStatusBar) View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            else View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        this.statusBarColor = statusBarColor
+    }
+}
+
+fun AppCompatActivity.marginTopAfterFullScreen(container: View, view: View?) {
+    ViewCompat.setOnApplyWindowInsetsListener(
+        container
+    ) { _, insets ->
+        view?.apply {
+            setPadding(
+                paddingLeft,
+                paddingTop + insets.systemWindowInsetTop,
+                paddingRight,
+                paddingBottom
+            )
+        }
+        insets.consumeSystemWindowInsets()
+    }
+}
+
+fun AppCompatActivity.hideStatusBar(root: View, isFitsSystemWindows: Boolean = true) {
+    WindowCompat.setDecorFitsSystemWindows(window, isFitsSystemWindows)
+    WindowInsetsControllerCompat(window, root).let { controller ->
+        controller.hide(WindowInsetsCompat.Type.statusBars())
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+    }
+}
+
+fun AppCompatActivity.showStatusBar(root: View, isFitsSystemWindows: Boolean = false) {
+    WindowCompat.setDecorFitsSystemWindows(window, isFitsSystemWindows)
+    WindowInsetsControllerCompat(window, root).show(WindowInsetsCompat.Type.statusBars())
 }
 
 fun AppCompatActivity.addOneTimeBackStackListener(onBackStackChange: () -> Unit) {
