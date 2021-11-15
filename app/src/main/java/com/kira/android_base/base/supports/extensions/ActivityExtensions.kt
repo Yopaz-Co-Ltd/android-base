9e85e45c891186a9e256abb12f892f718e40ebb1
tiendvyopaz <tiendv@yopaz.vn>
2021-11-15 16:09:41 +0700

    Complete make status bar transparent, hide or show status bar

--- app/src/main/java/com/kira/android_base/base/supports/extensions/ActivityExtensions.kt
+++ app/src/main/java/com/kira/android_base/base/supports/extensions/ActivityExtensions.kt
@@ -2,57 +2,71 @@
 
 package com.kira.android_base.base.supports.extensions
 
+import android.content.res.Configuration
 import android.graphics.Color
+import android.os.Build
 import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.fragment.app.FragmentManager
+import com.kira.android_base.R
 
-fun AppCompatActivity.makeStatusBarTransparent(
-    isLightStatusBar: Boolean = true,
-    statusBarColor: Int = Color.TRANSPARENT
+/**
+ * Before using, need to change theme and night-theme to NoActionBar
+ * */
+fun AppCompatActivity.enableStatusBarTransparent(
+    isLightStatusBar: Boolean = true
 ) {
     window?.apply {
         addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         decorView.systemUiVisibility =
             if (isLightStatusBar) View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
             else View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        this.statusBarColor = statusBarColor
+        this.statusBarColor = Color.TRANSPARENT
     }
 }
 
-fun AppCompatActivity.marginTopAfterFullScreen(container: View, view: View?) {
-    ViewCompat.setOnApplyWindowInsetsListener(
-        container
-    ) { _, insets ->
-        view?.apply {
-            setPadding(
-                paddingLeft,
-                paddingTop + insets.systemWindowInsetTop,
-                paddingRight,
-                paddingBottom
-            )
-        }
-        insets.consumeSystemWindowInsets()
+fun AppCompatActivity.disableStatusBarTransparent() {
+    window?.apply {
+        decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+
+        val nightModeFlags = context.resources.configuration.uiMode and
+                Configuration.UI_MODE_NIGHT_MASK
+        this.statusBarColor = context.getColor(
+            when (nightModeFlags) {
+                Configuration.UI_MODE_NIGHT_YES -> R.color.purple_200
+                else -> R.color.purple_500
+            }
+        )
     }
 }
 
 fun AppCompatActivity.hideStatusBar(root: View, isFitsSystemWindows: Boolean = true) {
-    WindowCompat.setDecorFitsSystemWindows(window, isFitsSystemWindows)
-    WindowInsetsControllerCompat(window, root).let { controller ->
-        controller.hide(WindowInsetsCompat.Type.statusBars())
-        controller.systemBarsBehavior =
-            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        WindowCompat.setDecorFitsSystemWindows(window, isFitsSystemWindows)
+        WindowInsetsControllerCompat(window, root).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.statusBars())
+            controller.systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    } else {
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
     }
 }
 
 fun AppCompatActivity.showStatusBar(root: View, isFitsSystemWindows: Boolean = false) {
-    WindowCompat.setDecorFitsSystemWindows(window, isFitsSystemWindows)
-    WindowInsetsControllerCompat(window, root).show(WindowInsetsCompat.Type.statusBars())
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        WindowCompat.setDecorFitsSystemWindows(window, isFitsSystemWindows)
+        WindowInsetsControllerCompat(window, root).show(WindowInsetsCompat.Type.statusBars())
+    } else {
+        window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    }
 }
 
 fun AppCompatActivity.addOneTimeBackStackListener(onBackStackChange: () -> Unit) {
