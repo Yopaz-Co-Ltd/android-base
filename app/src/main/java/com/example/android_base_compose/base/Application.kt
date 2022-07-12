24951ec93c27fa590c7a3cd895f16c30e5f20c5b
daihn <daihn@yopaz.vn>
2022-07-12 17:16:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android_base_compose/base/Application.kt
@@ -0,0 +1,5 @@
+package com.example.android_base_compose.base
+
+import android.app.Application
+
+class Application : Application()
---
+++ app/src/main/java/com/example/android_base_compose/main/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.android_base_compose.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import com.example.android_base_compose.main.theme.AndroidbasecomposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidbasecomposeTheme {
+                // A surface container using the 'background' color from the theme
+                Greeting("Android")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
