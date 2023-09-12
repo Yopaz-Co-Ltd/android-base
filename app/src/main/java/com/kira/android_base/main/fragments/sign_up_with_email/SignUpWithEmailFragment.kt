17782579b3b9fc5c45e8a0535bc29d24f9859d50
Trương Minh Nhật <nhattm@yopaz.vn>
2023-09-12 15:54:13 +0700

    task/20695 create ui sign in with email

--- app/src/main/java/com/kira/android_base/main/fragments/sign_up_with_email/SignUpWithEmailFragment.kt
+++ app/src/main/java/com/kira/android_base/main/fragments/sign_up_with_email/SignUpWithEmailFragment.kt
@@ -1,15 +1,9 @@
 package com.kira.android_base.main.fragments.sign_up_with_email
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.kira.android_base.R
 import com.kira.android_base.base.ui.BaseFragment
-import com.kira.android_base.databinding.FragmentHomeBinding
-import com.kira.android_base.main.MainViewModel
-import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class SignUpWithEmailFragment : BaseFragment(R.layout.fragment_sign_up_with_email) {
 
