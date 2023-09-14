12eff62b59ee22f846811ae47f235b3f6aa79d76
Trương Minh Nhật <nhattm@yopaz.vn>
2023-09-14 17:23:02 +0700

    Task 20694: Call api Login

--- app/src/main/java/com/kira/android_base/base/api/auth/models/login/LoginResponseModel.kt
+++ app/src/main/java/com/kira/android_base/base/api/auth/models/login/LoginResponseModel.kt
@@ -4,7 +4,7 @@ import com.kira.android_base.base.database.entities.User
 import com.squareup.moshi.Json
 
 data class LoginResponseModel(
-    @Json(name = "access_token")
+    @Json(name = "token")
     val accessToken: String?,
     val user: User?
 )
